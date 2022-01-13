3f89903de9961649f1d3ce20b38e187fd64e62f4
mygit <614800059@qq.com>
2022-01-13 23:12:16 +0800

    使用ctx结束goroutine

--- goroutine/context/context.go
+++ goroutine/context/context.go
@@ -106,9 +106,91 @@ func WithValue() {
 	fmt.Println("over")
 }
 
+// 使用自定义
+// var exitChan = make(chan bool, 1)
+
+// func f1() {
+// 	defer wg.Done()
+// FARLOOK:
+// 	for {
+// 		fmt.Println(time.Now().Format("05"))
+// 		time.Sleep(time.Millisecond * 500)
+// 		select {
+// 		case <-exitChan:
+// 			break FARLOOK
+// 		default:
+// 		}
+// 	}
+// }
+
+// func main() {
+// 	wg.Add(1)
+// 	go f1()
+// 	time.Sleep(time.Second * 5)
+// 	exitChan <- true
+// 	wg.Wait()
+// }
+
+func f2(ctx context.Context) {
+	defer wg.Done()
+FARLOOK:
+	for {
+		time.Sleep(time.Millisecond * 500)
+		fmt.Println("f2:", time.Now().Format("05"))
+		select {
+		case <-ctx.Done():
+			break FARLOOK
+		default:
+		}
+	}
+}
+
+func f1(ctx context.Context) {
+	defer wg.Done()
+	go f2(ctx)
+FARLOOK:
+	for {
+		time.Sleep(time.Millisecond * 500)
+		fmt.Println("f1:", time.Now().Format("05"))
+		select {
+		case <-ctx.Done():
+			break FARLOOK
+		default:
+		}
+	}
+}
+
+func sel(g coroutine, i int) {
+	fmt.Println(i)
+	if i == 5 {
+		g.err <- struct{}{}
+		g.wg.Done()
+	}
+	g.wg.Done()
+}
+
+type coroutine struct {
+	wg  *sync.WaitGroup
+	ctx context.Context
+	err chan struct{}
+}
+
 func main() {
-	// WithCancel()
-	// WithDeadline()
-	// WithTimeout()
-	WithValue()
+	ctx, cancel := context.WithCancel(context.Background()) //使用ctx结束goroutine
+
+	routine := coroutine{
+		ctx: ctx,
+		wg:  &sync.WaitGroup{},
+		err: make(chan struct{}),
+	}
+	for i := 0; i < 10; i++ {
+		routine.wg.Add(1)
+		go sel(routine, i)
+	}
+
+	select {
+	case <-routine.err:
+		cancel()
+	}
+	wg.Wait()
 }
